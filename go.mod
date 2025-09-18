f1d28e4eedba38f3b96371199c4388ee1f5110d3
Frank Mayer <gh@frankmayer.dev>
2025-09-19 01:33:37 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tsukinoko-kun/goreleasertest
+
+go 1.25.1
+
+require github.com/DataDog/zstd v1.5.7
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/DataDog/zstd"
+)
+
+func main() {
+	in := []byte("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+	out, err := zstd.Compress(nil, in)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("in:", base64.StdEncoding.EncodeToString(in))
+	fmt.Println()
+	fmt.Println("out:", base64.StdEncoding.EncodeToString(out))
+}
